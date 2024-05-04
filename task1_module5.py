c57a9a3c12d9c28560c17c8604ebbfde1f6d0b68
mzLeVit <160911095+mzLeVit@users.noreply.github.com>
2024-05-04 14:04:59 +0300

    Add files via upload

---
+++ task1_module5.py
@@ -0,0 +1,18 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+fibonacci = caching_fibonacci()
+print(fibonacci(15))
